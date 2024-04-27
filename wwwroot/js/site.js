ffac12859a140b832f9df584b477ee434680e161
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2024-04-27 11:23:59 +0500

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// JavaScript animation example
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.querySelector('iframe');
+    element.addEventListener('mouseover', function () {
+        element.style.transform = 'scale(1.1)';
+    });
+    element.addEventListener('mouseout', function () {
+        element.style.transform = 'scale(1)';
+    });
+});
